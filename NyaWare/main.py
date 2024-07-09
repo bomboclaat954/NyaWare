abc87758fdd17554383cf33777dfa12e2a5988fa
meloodi <84985788+YTKotel@users.noreply.github.com>
2024-07-09 21:55:37 +0200

    Update main.py

--- NyaWare/main.py
+++ NyaWare/main.py
@@ -11,7 +11,7 @@ from win32file import *
 from plyer import notification
 import shutil
 
- if not ctypes.windll.shell32.IsUserAnAdmin():
+if not ctypes.windll.shell32.IsUserAnAdmin():
     ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, ' '.join(sys.argv), None, 1)
     sys.exit()
 
